cc6b94cb49e8a36b5e373538b2e7ddaec3f756c5
Gunjan Soni <gunjan.soni2002@gmail.com>
2016-03-14 03:23:36 +0530

    Summary to be less than 100 chars

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-    name         : 'gunjansoni:html5-api',
-    version      : '0.1.0',
-    summary      : 'Rich HTML5 APIs wrapped for using with Meteor including Desktop Notifications, Geo Location, Network Information, Page Visibility, Speech Synthesis, Speech Recognition and Mobile Vibration',
-    git          : 'https://github.com/guns2410/meteor-html5-apis',
+    name: 'gunjansoni:html5-api',
+    version: '0.1.0',
+    summary: 'Notifications, Location, Network, Page Visibility, Speech Synthesis, Speech Recognition, Vibration',
+    git: 'https://github.com/guns2410/meteor-html5-apis',
     documentation: 'README.md'
 });
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
     api.versionsFrom('1.2.1');
-    api.use([ 'ecmascript', 'mongo', 'underscore', 'jsx@0.2.3' ]);
+    api.use(['ecmascript', 'mongo', 'underscore', 'jsx@0.2.3']);
     api.addFiles('lib/GeoLocation.jsx', 'client');
     //api.addFiles('lib/GetUserMedia.jsx', 'client');
     api.addFiles('lib/NetworkInformation.jsx', 'client');
@@ -21,9 +21,9 @@ Package.onUse(function (api) {
     api.export("Html5Api");
 });
 
-Package.onTest(function (api) {
+Package.onTest(function(api) {
     api.use('ecmascript');
     api.use('tinytest');
-    api.use('gunjansoni:html5-apis');
+    api.use('gunjansoni:html5-api');
     api.addFiles('html5-apis-tests.js');
 });
