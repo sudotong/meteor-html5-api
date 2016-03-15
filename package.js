797f8724acbc8c192ff5ad92ee6024c74ffd383d
Gunjan Soni <gunjan.soni2002@gmail.com>
2016-03-15 21:15:03 +0530

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'gunjansoni:html5-api',
-    version: '0.1.0',
+    version: '0.1.2',
     summary: 'Notifications, Location, Network, Page Visibility, Speech Synthesis, Speech Recognition, Vibration',
     git: 'https://github.com/guns2410/meteor-html5-api',
     documentation: 'README.md'
